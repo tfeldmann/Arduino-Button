fd72f07ea9d574b7ba49afc2b849034f70832622
Thomas Feldmann <mail@tfeldmann.de>
2023-01-09 17:32:03 +0100

    add library spec

--- examples/BasicUsage.ino
+++ examples/BasicUsage.ino
@@ -14,12 +14,12 @@ void loop()
     btn2.update();
 
     if (btn1.just_pressed())
-        Serial.println("Pressed");
+        Serial.println("Btn 1 pressed");
     if (btn1.just_released())
-        Serial.println("Released");
+        Serial.println("Btn 1 released");
 
     if (btn2.just_pressed())
-        Serial.println("Pressed");
+        Serial.println("Btn 2 pressed");
     if (btn2.just_released())
-        Serial.println("Released");
+        Serial.println("Btn 2 released");
 }
